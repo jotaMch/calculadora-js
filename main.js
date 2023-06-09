81d7603cb789493a9ecff59f97fcd096cdfcc27b
Jânderson Machado <jandersonmachado090@gmail.com>
2023-06-08 21:51:24 -0300

    adicionando js com comentario

--- main.js
+++ main.js
@@ -7,9 +7,16 @@ function insert(num) {
     }
 
 function calcular() {
-    var resultado = document.getElementById('valor').value;
+    var resultado = document.getElementById('valor').value; // pega o valor do input
 
     if(resultado) {
-        document.getElementById('valor').value = eval(resultado);
+        document.getElementById('valor').value = eval(resultado); 
     }
 }
+
+var resultado = document.getElementById('valor');
+function validateInput(resultado) {
+    resultado.value = resultado.value.replace(/[^0-9+\-*/.,]/g, ''); //substitui os elementos
+}
+
+validateInput();
